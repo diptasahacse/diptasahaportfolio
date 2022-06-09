3cb0aa5413ed3ca0cde0019f0edab09e88991936
diptasahacse <diptasaha.lpu.cse@gmail.com>
2022-06-09 23:21:50 +0600

    update json

--- src/components/Home/Projects/ProjectCard/ProjectCard.js
+++ src/components/Home/Projects/ProjectCard/ProjectCard.js
@@ -1,15 +1,18 @@
 import React from 'react';
 
 const ProjectCard = ({ project }) => {
-    const { des, id, img, name, shortDes } = project;
+    const { des, id, img, name, shortDes, live } = project;
     return (
         <div class="card card-compact bg-base-100 shadow-xl">
-            <figure><img src={img} alt="Shoes" /></figure>
+            <figure><img src={img} className="w-full h-60" alt="Shoes" /></figure>
             <div class="card-body">
                 <h2 class="card-title">{name}</h2>
                 <p>{shortDes}</p>
-                <div class="card-actions justify-end mt-5">
-                    <button class="btn btn-primary">Details</button>
+                <div class="mt-5">
+                    <div className='flex justify-between'>
+                        <a class="btn btn-primary btn-sm" href={live} target='_blank'>Live</a>
+                        <button class="btn btn-primary btn-sm">Details</button>
+                    </div>
                 </div>
             </div>
         </div>
