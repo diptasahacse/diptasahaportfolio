426e2cf04a78653b383bcbafdae60a2706f7f8f3
diptasahacse <diptasaha.lpu.cse@gmail.com>
2022-06-10 14:26:00 +0600

    add technology in json file

--- src/components/Home/Projects/ProjectCard/ProjectCard.js
+++ src/components/Home/Projects/ProjectCard/ProjectCard.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const ProjectCard = ({ project }) => {
-    const { des, id, img, name, shortDes, live } = project;
+    const { des, id, img, name, shortDes, live, technology } = project;
     return (
         <div className="card card-compact bg-base-100 shadow-xl">
             <figure><img src={img} className="w-full h-60" alt="Shoes" /></figure>
@@ -9,6 +9,11 @@ const ProjectCard = ({ project }) => {
                 <h2 className="card-title">{name}</h2>
                 <p>{shortDes}</p>
                 <div className="mt-5">
+                    <div className='mb-4'>
+                        {
+                            technology.map((tech, index) => <div key={index} class="badge m-1 bg-green-600 border-0 text-white">{tech}</div>)
+                        }
+                    </div>
                     <div className='flex justify-between'>
                         <a className="btn btn-primary btn-sm" href={live} target='_blank'>Live</a>
                         <button disabled className="btn btn-primary btn-sm">Details</button>
